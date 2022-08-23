de2f1d60f3d562bd4317213fb438d57d199c9651
Lini Lorance <lini.lorance@codinghands.in>
2022-08-23 14:37:41 +0530

    buy two get one offer

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,23 +1,26 @@
 import { Button, Typography } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
 
 const Cart = (props) => {
   const { cartItems, onAdd, onRemove, setCartItems, products } = props;
   const itemsPrice = cartItems?.reduce((a, c) => a + c.qty * c.price, 0);
   const find = cartItems.find((i) => i.id == 2)?.qty > 2;
-  // console.log("products", products, "cartItem", cartItems,"find",find);
 
-  const totalPrice = find ? itemsPrice - 5.6 : itemsPrice;
-  // console.log("itemPrice", itemsPrice, "find", find);
+  const fruittea = Math.ceil((cartItems.map((i) => i.qty) / 3) * 2);
+  const totalPrice2 = find ? itemsPrice - 5.6 : itemsPrice;
+
+  const totalPrice1 =
+    cartItems.find((i) => i.id == 1) &&
+    fruittea * cartItems.map((i) => i.price);
+  const totalPrice =  cartItems.find((i) => i.id == 1)&&totalPrice1 ||totalPrice2|| itemsPrice;
+ 
+
   return (
     <>
       {cartItems.length === 0 && <Typography>Cart is empty</Typography>}
       {cartItems.map((item) => (
         <div key={item.id}>
           <h4>{item.name}</h4>
-          <Typography>
-            Quantity:{item.id == 1 ? item.qty*2: item.qty}
-          </Typography>
 
           <div>
             {item.qty} x ${item.price}
@@ -36,7 +39,6 @@ const Cart = (props) => {
       {cartItems.length !== 0 && (
         <>
           <h3>Total Price: ${totalPrice}</h3>
-
           <Button variant="contained" onClick={() => setCartItems([])}>
             Clear
           </Button>
